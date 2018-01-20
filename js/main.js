074ce30060df5ed2d4515ad8f61c6a754a695150
Mei Chen <meichen@Meis-MacBook-Pro.local>
2018-01-20 14:38:01 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo =  "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+console.log("User flipped queen");
+var cardOne = cards[1]; //queen
+cardsInPlay.push(cardOne); //cardsInPlay["queen"]
+var cardTwo = cards[2]; //king 
+cardsInPlay.push(cardTwo); //cardsInPlay["queen", "king"]
+console.log("User flipped king");
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try againß");
+	}
+}
